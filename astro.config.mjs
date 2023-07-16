8f714402e26dedb5e2e88da01fc352cf6e49c7d6
langermank <18661030+langermank@users.noreply.github.com>
2023-07-16 10:37:55 -0700

    test og

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "fastidious-chaja-c7d530.netlify.app",
+});
